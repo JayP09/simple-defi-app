c8a913f2f69ea707212b95dbb479f19e1caf5bad
Jay Panchal <panchalj272@gmail.com>
2022-01-18 22:35:25 +0530

    Updated scripts files

--- scripts/run.js
+++ scripts/run.js
@@ -22,6 +22,16 @@ async function main() {
 
   await daiTokenContract.transfer(addr1.address,'100000000000000000000');
 
+  await daiTokenContract.connect(addr1).approve(tokenFarmContract.address,'100000000000000000000');
+
+  await (await tokenFarmContract.connect(addr1).stakeTokens('100000000000000000000')).wait();
+
+  console.log("staking Balance",await tokenFarmContract.stakingBalance(addr1.address));
+
+  await (await tokenFarmContract.connect(addr1).unstakeTokens('100000000000000000000')).wait();
+
+  console.log(await dappTokenContract.balanceOf(addr1.address));
+
   console.log(owner.address)
   console.log(addr1.address)
 }
